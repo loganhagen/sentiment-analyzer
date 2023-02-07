31a48678d883e29ed7100b0814838532e7fc163f
klukasze <klukasze@uoguelph.ca>
2023-02-07 14:16:39 -0500

    Added +layout.svelte so that TailwindCSS is properly configured for the frontend application

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-slate-500 text-4xl px-2 py-4">Welcome to SvelteKit</h1>
+<p class="px-2">
+	Visit <a class="underline text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	documentation
+</p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.800);
+	}
+</style>
