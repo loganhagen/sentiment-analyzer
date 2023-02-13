1051be1dd332371dc7cc44670a6830aa5a602959
lhagen02 <lhagen02@uoguelph.ca>
2023-02-13 16:06:02 -0500

    Can get random tweet on button click.

--- frontend/src/routes/home.svelte
+++ frontend/src/routes/home.svelte
@@ -1,12 +1,33 @@
 <script>
-	let totalPosts = 6;
+	import { onMount } from "svelte";
+	let totalPosts = -1;
+	let random = "N/A"
+
+	const sizeEndpoint = "http://127.0.0.1:6969/size";
+	onMount(async function() {
+		const sizeResponse = await fetch(sizeEndpoint);
+		const sizeJSON = await sizeResponse.json()
+		totalPosts = sizeJSON["size"]
+		
+	});
+
+	async function getRandomTweet() {
+		const response = await fetch("http://127.0.0.1:6969/random")
+		const responseJSON = await response.json()
+
+		random = responseJSON["tweet"]
+	}
+
+
 </script>
 
 <main>
 	<h1 class="px-2 py-4 text-4xl text-slate-500">Universal Basic Income</h1>
-	<p class="px-2">
-		Total Posts: {totalPosts}
-	</p>
+	<p class="px-2"> Total Posts: {totalPosts}</p>
+	<button on:click={getRandomTweet}>Get Random Tweet</button>
+	<p>{random}</p>
+
+
 </main>
 
 <style lang="postcss">
