709a4884be1dee268ae4280c753a9c794778051f
lhagen02 <lhagen02@uoguelph.ca>
2023-02-13 15:31:25 -0500

    Added fetch requests for collection size and random post from database.

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@
 import os
 import tweetList
 from flask import Flask
+import flask
 
 app = Flask(__name__)
 
@@ -14,15 +15,19 @@ def hello_world():
     return "home"
 
 @app.route("/random", methods = ["GET"])
-def tweet():
-    """tweet"""
+def randomTweet():
+    """randomTweet"""
     doc = t.getRandomDocument("UBI", "tweets")
+    response = flask.jsonify({"tweet" : str(doc["content"])})
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return doc["content"]
+    return response
 
 @app.route("/size", methods = ["GET"])
 def size():
     """size"""
     collectionSize = t.getCollectionSize("UBI", "tweets")
+    response = flask.jsonify({"size" : collectionSize})
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return str(collectionSize)
+    return response
