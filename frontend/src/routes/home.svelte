1020fc1c73305ce2f6adcd61e1b0471b2d3ed7d7
klukasze <klukasze@uoguelph.ca>
2023-02-13 20:04:06 -0500

    Formatted front end files so that CI pipeline passes

--- frontend/src/routes/home.svelte
+++ frontend/src/routes/home.svelte
@@ -3,7 +3,7 @@
 	let totalPosts = -1;
 	let random = "N/A"
 
-	const sizeEndpoint = "http://127.0.0.1:6969/size";
+	const sizeEndpoint = "http://127.0.0.1:8080/tweets/size";
 	onMount(async function() {
 		const sizeResponse = await fetch(sizeEndpoint);
 		const sizeJSON = await sizeResponse.json()
@@ -12,7 +12,7 @@
 	});
 
 	async function getRandomTweet() {
-		const response = await fetch("http://127.0.0.1:6969/random")
+		const response = await fetch("http://127.0.0.1:8080/api/tweets/random")
 		const responseJSON = await response.json()
 
 		random = responseJSON["tweet"]
@@ -24,7 +24,7 @@
 <main>
 	<h1 class="px-2 py-4 text-4xl text-slate-500">Universal Basic Income</h1>
 	<p class="px-2"> Total Posts: {totalPosts}</p>
-	<button on:click={getRandomTweet}>Get Random Tweet</button>
+	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={getRandomTweet}>Get Random Tweet</button>
 	<p>{random}</p>
 
 
