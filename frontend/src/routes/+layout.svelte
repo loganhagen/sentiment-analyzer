998fc3dc10af844996dff44a035f35b386de63c4
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-02-14 18:47:24 -0500

    Refactored routes and navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,4 +2,31 @@
 	import '../app.css';
 </script>
 
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+	<div class="container flex flex-wrap items-center justify-between mx-auto">
+		<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+			<ul
+				class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+			>
+				<li>
+					<a
+						href="/home"
+						class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent mod:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+					>
+						Home
+					</a>
+				</li>
+				<li>
+					<a
+						href="/about"
+						class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+					>
+						About
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
 <slot />
