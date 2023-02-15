6f11fb6cacffd3b4f759373981aa0d2ddfc52028
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-02-14 19:17:32 -0500

    Created components for getting random tweett and size of database

---
+++ frontend/src/routes/home/random.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	let random = 'N/A';
+
+	async function getRandomTweet() {
+		const response = await fetch('http://127.0.0.1:8080/api/tweets/random');
+		const responseJSON = await response.json();
+		console.log(responseJSON);
+		random = responseJSON['tweet'];
+	}
+</script>
+
+<main>
+	<button
+		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		on:click={getRandomTweet}>Get Random Tweet</button
+	>
+	<p>{random}</p>
+</main>
---
+++ frontend/src/routes/home/size.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+	let totalPosts = -1;
+
+	const sizeEndpoint = 'http://127.0.0.1:8080/api/tweets/size';
+	onMount(async function () {
+		const sizeResponse = await fetch(sizeEndpoint);
+		const sizeJSON = await sizeResponse.json();
+		totalPosts = sizeJSON['size'];
+	});
+</script>
+
+<main>
+	<p class="px-2">Total Posts: {totalPosts}</p>
+</main>
