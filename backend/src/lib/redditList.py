7860c74e3915d596c9049456f94e906327ef21b7
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-03 14:38:27 -0500

    chore: Add environment variables

--- backend/src/lib/redditList.py
+++ backend/src/lib/redditList.py
@@ -1,25 +1,24 @@
+import os
+from pymongo import MongoClient
 import requests
 import json
 
 
 #add environment variables for reddit username, password, client_id and secret_token
 
-with open('password.json', 'r') as f:
-	passwordData = json.load(f)
-
-print(passwordData)
 
 
-
-auth = requests.auth.HTTPBasicAuth(passwordData['CLIENT_ID'], passwordData['SECRET_TOKEN'])
+auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
 
 data = {
 	'grant_type': 'password',
-	'username': passwordData['USERNAME'],
-	'password': passwordData['PASSWORD']
+	'username': os.environ.get("REDDIT_USERNAME"),
+	'password': os.environ.get("REDDIT_PASSWORD")
 }
 
 headers = {'User-Agent': 'dataFetcher'}
+
+print(data)
 
 res = requests.post('https://www.reddit.com/api/v1/access_token',auth=auth, data=data, headers=headers)
 
