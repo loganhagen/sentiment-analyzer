44fbb6a277409f5141663346d23c0491ac2f57cc
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-04 00:17:34 -0500

    chore: Begin writing Reddit API class

--- backend/src/lib/redditList.py
+++ backend/src/lib/redditList.py
@@ -1,20 +1,54 @@
 import os
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 import requests
 import json
+import mongomock
 
 
 #add environment variables for reddit username, password, client_id and secret_token
 
-
-
-auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
+mongo_uri = 'mongodb://' + os.environ.get('MONGO_USERNAME') + ':' + os.environ.get('MONGO_PASSWORD') + '@' + os.environ.get('MONGO_HOSTNAME') + ':27017'
+test_db = os.environ.get('DB_TEST')
 
 data = {
 	'grant_type': 'password',
 	'username': os.environ.get("REDDIT_USERNAME"),
 	'password': os.environ.get("REDDIT_PASSWORD")
 }
+
+class RedditList:
+	"""Class that handles Reddit API"""
+
+	def __init__(self):
+		"""Initialize"""
+		self.mongo_client = None
+
+		# Return a mock database to the client variable is the DB_TEST env variable is raised.
+		if test_db == '1':
+			self.mongo_client = mongomock.MongoClient()
+		else:
+			self.mongo_client = MongoClient(mongo_uri)
+
+	def getMongoClientStatus(self):
+		try:
+			self.mongo_client.admin.command('ismaster')
+		except ConnectionFailure:
+			return "Server not available"
+		return "Server available"
+
+	def getSubredditPosts(self):
+		return None
+
+	def getComments(self):
+		return None
+	
+
+
+
+auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
+
+
 
 headers = {'User-Agent': 'dataFetcher'}
 
