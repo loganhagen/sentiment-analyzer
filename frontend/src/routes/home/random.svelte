4c462b2692cc7f597af7e53cb58267edc5f55aa3
klukasze <klukasze@uoguelph.ca>
2023-03-03 21:51:58 -0500

    Fixed commit message problem, see commit notes in comments below.

--- frontend/src/routes/home/random.svelte
+++ frontend/src/routes/home/random.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-	let random = 'N/A';
 
-	async function getRandomTweet() {
-		const response = await fetch('http://localhost:8080/api/tweets/random');
-		const responseJSON = await response.json();
-		console.log(responseJSON);
-		random = responseJSON['tweet'];
+	import { getRandomTweet, cur_tweet } from "./+page.svelte"
+
+	//set tweet_text for when page loads
+	let tweet_text:string = cur_tweet.tweet_text;
+
+	//Get a new random tweet and set the text on the page to match the tweet.
+	//The tweet is saved in ./+page.svelte as 'cur_tweet' which can be imported into different files.
+	async function ButtonUpdateComponent() {
+		await getRandomTweet();
+		tweet_text = cur_tweet.tweet_text;
 	}
+
 </script>
 
 <main>
 	<button
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-		on:click={getRandomTweet}>Get Random Tweet</button
+		on:click={ButtonUpdateComponent}>Get Random Tweet</button
 	>
-	<p>{random}</p>
+	<p>{tweet_text}</p>
 </main>
