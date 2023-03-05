3e99169126f30752c9fc292ffafd2aae16656d1a
lhagen02 <lhagen02@uoguelph.ca>
2023-03-05 16:37:05 -0500

    feat: create function to censor tweets

--- backend/db_operations.py
+++ backend/db_operations.py
@@ -2,17 +2,11 @@
 from profanity_filter import ProfanityFilter
 from src.lib.tweetList import TweetList
 
-t = TweetList()
-pf = ProfanityFilter()
+def censorTweets(database, collection):
+    t = TweetList()
+    pf = ProfanityFilter()
+    db = t.mongo_client[database]
+    col = db[collection]
 
-db = t.mongo_client["UBI"]
-col = db["tweets"]
-
-t.writeCollectionToJSON("UBI", "tweets", "tweets.json")
-
-# for document in col.find({}):
-#     str = document["content"]
-#     str = pf.censor(str)
-
-#     col.update_one(document, {"$set": {"content" : str}})
-
+    for document in col.find({}):
+        col.update_one(document, {"$set": {"content" : pf.censor(document["content"])}})
