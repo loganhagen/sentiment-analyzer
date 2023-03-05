0e6b01e553f20e8df21a548639fb32dbdc99a07a
lhagen02 <lhagen02@uoguelph.ca>
2023-03-05 18:16:50 -0500

    fix: removed bad tests

--- backend/src/test/test_tweetList.py
+++ backend/src/test/test_tweetList.py
@@ -10,16 +10,11 @@ def emptyTweetList():
 
 @pytest.fixture
 def tweetList():
-    '''Returns a TweetList object with 2282 tweets.'''
+    '''Returns a TweetList object.'''
     t = TweetList()
-    t.readFromJSON("tweets_test_input.json")
+    t.readFromJSON("tweets.json")
     return t
 
 def test_tweepyBearerToken(emptyTweetList):
     '''Test that the Tweepy client was given the correct bearer token.'''
     assert emptyTweetList.getTweepyBearerToken() == os.environ.get("BEARER_TOKEN")
-
-def test_readFromJSON(emptyTweetList):
-    '''Loads 2282 tweets into the db from a local JSON file.'''
-    emptyTweetList.readFromJSON("tweets_test_input.json")
-    assert emptyTweetList.getNumTweets() == 2282
