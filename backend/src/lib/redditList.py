d1e0afad35db7abf82e76954b3790c2cd1d0b1b9
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-06 14:04:55 -0500

    feat: Get hot posts from basicincome subreddit

--- backend/src/lib/redditList.py
+++ backend/src/lib/redditList.py
@@ -1,9 +1,10 @@
 import os
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
 import requests
 import json
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 import mongomock
+
 
 
 #add environment variables for reddit username, password, client_id and secret_token
@@ -11,17 +12,16 @@ import mongomock
 mongo_uri = 'mongodb://' + os.environ.get('MONGO_USERNAME') + ':' + os.environ.get('MONGO_PASSWORD') + '@' + os.environ.get('MONGO_HOSTNAME') + ':27017'
 test_db = os.environ.get('DB_TEST')
 
-data = {
-	'grant_type': 'password',
-	'username': os.environ.get("REDDIT_USERNAME"),
-	'password': os.environ.get("REDDIT_PASSWORD")
-}
+
 
 class RedditList:
 	"""Class that handles Reddit API"""
 
 	def __init__(self):
 		"""Initialize"""
+		self.headers = {'User-Agent': 'dataFetcher'}
+		self.res = self.getOAuthToken()
+
 		self.mongo_client = None
 
 		# Return a mock database to the client variable is the DB_TEST env variable is raised.
@@ -29,6 +29,17 @@ class RedditList:
 			self.mongo_client = mongomock.MongoClient()
 		else:
 			self.mongo_client = MongoClient(mongo_uri)
+
+	def getOAuthToken(self):
+		"""Returns OAuth token from Reddit API"""
+		auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
+		data = {
+			'grant_type': 'password',
+			'username': os.environ.get("REDDIT_USERNAME"),
+			'password': os.environ.get("REDDIT_PASSWORD")
+		}
+		res = requests.post('https://www.reddit.com/api/v1/access_token',auth=auth, data=data, headers=self.headers, timeout=5)
+		return res
 
 	def getMongoClientStatus(self):
 		try:
@@ -38,36 +49,24 @@ class RedditList:
 		return "Server available"
 
 	def getSubredditPosts(self):
+		#hot 
+		self.getHotPosts()
+		#new 
+		#rising
 		return None
+	
+	def getHotPosts(self):
+
+		TOKEN = self.res.json()['access_token']
+		self.headers = {**self.headers, **{'Authorization': f"bearer {TOKEN}"}}
+		redRes = requests.get('https://oauth.reddit.com/r/basicincome/hot',headers=self.headers,timeout=5)
+		for post in redRes.json()['data']['children']:
+			print(post['data']['title'])
 
 	def getComments(self):
 		return None
 	
 
-
-
-auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
-
-
-
-headers = {'User-Agent': 'dataFetcher'}
-
-print(data)
-
-res = requests.post('https://www.reddit.com/api/v1/access_token',auth=auth, data=data, headers=headers)
-
-TOKEN = res.json()['access_token']
-
-headers['Authorization'] = f'bearer {TOKEN}'
-
-#print(headers)
-
-#print(requests.get('https://oauth.reddit.com/api/v1/me', headers=headers))
-
-redRes = requests.get('https://oauth.reddit.com/r/basicincome/hot',headers=headers)
-
-#print(redRes.json())
-
-for post in redRes.json()['data']['children']:
-	print(post['data']['title'])
-	
+if __name__ == "__main__":
+	reddit = RedditList()
+	reddit.getHotPosts()
