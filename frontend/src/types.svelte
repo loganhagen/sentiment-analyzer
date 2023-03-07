d73e1201ce70ef675586bb0670f2d21c19b2e6da
klukasze <klukasze@uoguelph.ca>
2023-03-06 23:58:45 -0500

    fix: Forgot to add types.svelte to the branch repo.

---
+++ frontend/src/types.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+	//Tweet data structure containing tweet text and tweet id. id is used in fetch requests so it is important the frontend has access to it.
+	export type Post = {
+		post_text: string;
+		post_id: number;
+		post_date: string;
+		post_type: string;
+	};
+
+	//Plot data structure containing plotly data and plotly layout as objects from JSON.
+	export type Plot = {
+		data: object;
+		layout: object;
+	};
+</script>
