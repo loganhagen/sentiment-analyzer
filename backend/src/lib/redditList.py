9b040355d6f5ab4f1da128f1c7d748e7cef73233
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-06 20:43:39 -0500

    feat: get new and top posts

--- backend/src/lib/redditList.py
+++ backend/src/lib/redditList.py
@@ -1,72 +1,143 @@
 import os
+from datetime import datetime
 import requests
-import json
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
-import mongomock
-
-
-
-#add environment variables for reddit username, password, client_id and secret_token
-
-mongo_uri = 'mongodb://' + os.environ.get('MONGO_USERNAME') + ':' + os.environ.get('MONGO_PASSWORD') + '@' + os.environ.get('MONGO_HOSTNAME') + ':27017'
-test_db = os.environ.get('DB_TEST')
 
 
 
 class RedditList:
-	"""Class that handles Reddit API"""
+    """Class that handles Reddit API"""
 
-	def __init__(self):
-		"""Initialize"""
-		self.headers = {'User-Agent': 'dataFetcher'}
-		self.res = self.getOAuthToken()
+    def __init__(self):
+        """Initialize"""
+        self.headers = {'User-Agent': 'dataFetcher'}
+        self.res = self.getOAuthToken()
+        self.postList = []
+        self.commentList = []
+        self.query_result = None
+        self.mongo_client = None
 
-		self.mongo_client = None
 
-		# Return a mock database to the client variable is the DB_TEST env variable is raised.
-		if test_db == '1':
-			self.mongo_client = mongomock.MongoClient()
-		else:
-			self.mongo_client = MongoClient(mongo_uri)
 
-	def getOAuthToken(self):
-		"""Returns OAuth token from Reddit API"""
-		auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
-		data = {
-			'grant_type': 'password',
-			'username': os.environ.get("REDDIT_USERNAME"),
-			'password': os.environ.get("REDDIT_PASSWORD")
-		}
-		res = requests.post('https://www.reddit.com/api/v1/access_token',auth=auth, data=data, headers=self.headers, timeout=5)
-		return res
+    def getOAuthToken(self):
+        """Returns OAuth token from Reddit API"""
+        auth = requests.auth.HTTPBasicAuth(os.environ.get("CLIENT_ID"), os.environ.get("SECRET_TOKEN"))
+        data = {
+            'grant_type': 'password',
+            'username': os.environ.get("REDDIT_USERNAME"),
+            'password': os.environ.get("REDDIT_PASSWORD")
+        }
+        #may have to write a function to refresh the token when it expires for asynchronous calls to reddit api when the program is hosted on kubernetes
+        res = requests.post('https://www.reddit.com/api/v1/access_token',auth=auth, data=data, headers=self.headers, timeout=5)
+        TOKEN = res.json()['access_token']
+        self.headers = {**self.headers, **{'Authorization': f"bearer {TOKEN}"}}
+        return res
 
-	def getMongoClientStatus(self):
-		try:
-			self.mongo_client.admin.command('ismaster')
-		except ConnectionFailure:
-			return "Server not available"
-		return "Server available"
 
-	def getSubredditPosts(self):
-		#hot 
-		self.getHotPosts()
-		#new 
-		#rising
-		return None
-	
-	def getHotPosts(self):
 
-		TOKEN = self.res.json()['access_token']
-		self.headers = {**self.headers, **{'Authorization': f"bearer {TOKEN}"}}
-		redRes = requests.get('https://oauth.reddit.com/r/basicincome/hot',headers=self.headers,timeout=5)
-		for post in redRes.json()['data']['children']:
-			print(post['data']['title'])
+    
+    def getHotPosts(self):
+        """Adds hot posts to postList"""
+        redRes = requests.get('https://oauth.reddit.com/r/basicincome/hot',params={'limit': '2'},headers=self.headers,timeout=5)
+        self.query_result = redRes.json()['data']['children']
+        self.initPostList()
 
-	def getComments(self):
-		return None
-	
+
+    def getNewPosts(self):
+        """Adds new posts to postList"""	
+
+        redRes = requests.get('https://oauth.reddit.com/r/basicincome/new',params={'limit': '100'},headers=self.headers,timeout=5)
+        self.query_result = redRes.json()['data']['children']
+        self.initPostList()
+
+
+
+    def getTopPost(self):
+        redRes = requests.get('https://oauth.reddit.com/r/basicincome/top',params={'limit': '100','t': 'all'},headers=self.headers,timeout=5)
+        self.query_result = redRes.json()['data']['children']
+        self.initPostList()
+
+
+    def initPostList(self):
+        """Adds posts to postList"""
+        for child in self.query_result:
+            title = child['data']['title']
+            author = child['data']['author']
+            created_utc = child['data']['created_utc']
+            post_id = child['data']['id']
+
+            #check if post exists in post list
+            #i think this should be done in the database file instead
+            if not self.postExists(post_id):
+                self.postList.append(
+                    self.Post(
+                        title,
+                        author,
+                        post_id,
+                        created_utc
+                    )
+                )
+
+    def getPostList(self):
+        """Returns the post list"""
+        return self.postList
+
+    def postExists(self, post_id):
+        """Returns true if post exists in postList"""
+        for post in self.postList:
+            if post.post_id == post_id:
+                return True
+        return False
+
+    def getNumPosts(self):
+        """Returns total number of posts"""
+        return len(self.postList)
+
+    def getComments(self):
+        """Gets a list of comments for each post in postList"""
+        if not self.postList:
+            return False
+    
+        return None
+    
+    class Post:
+        def __init__(self, title, author, post_id, created_utc):
+            self.title = title
+            self.author = author
+            self.post_id = post_id
+            self.date = datetime.fromtimestamp(created_utc)
+            self.commentList = []
+
+        def getPostID(self):
+            return self.post_id
+        
+        def getTitle(self):
+            return self.title
+        
+        def getDate(self):
+            return self.date
+        
+        def toDict(self):
+            """Returns dictionary representation of Post"""
+            return {
+                'title': str(self.title),
+                'author': str(self.author),
+                'post_id': str(self.post_id),
+                'date': str(self.date)
+            }
+        
+    class Comment:
+        def __init__(self, author, comment, post_id, created_utc):
+            self.author = author
+            self.comment = comment
+            self.post_id = post_id
+            self.date = created_utc
+      
+        def getComment(self):
+            return self.comment
+
+    
 
 if __name__ == "__main__":
-	reddit = RedditList()
-	reddit.getHotPosts()
+    reddit = RedditList()
+    reddit.getTopPost()
+    print(reddit.getNumPosts())
