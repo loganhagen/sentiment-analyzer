1f707c3fbfe7b0f74416d95ba14c3dfcecde3559
lhagen02 <lhagen02@uoguelph.ca>
2023-03-08 20:54:19 -0500

    fix: merge conflicts

--- frontend/src/routes/home/random.svelte
+++ frontend/src/routes/home/random.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 
-	import { getRandomTweet, cur_tweet } from "./+page.svelte"
+	import { getRandomTweet, cur_post } from "./+page.svelte"
 
 	//set tweet_text for when page loads
-	let tweet_text:string = cur_tweet.tweet_text;
+	let post_text:string = cur_post.post_text;
 
 	//Get a new random tweet and set the text on the page to match the tweet.
 	//The tweet is saved in ./+page.svelte as 'cur_tweet' which can be imported into different files.
 	async function ButtonUpdateComponent() {
 		await getRandomTweet();
-		tweet_text = cur_tweet.tweet_text;
+		post_text = cur_post.post_text;
 	}
 
 </script>
@@ -17,7 +17,7 @@
 <main>
 	<button
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-		on:click={ButtonUpdateComponent}>Get Random Tweet</button
+		on:click={ButtonUpdateComponent}>Get Random Post</button
 	>
-	<p>{tweet_text}</p>
+	<p>{post_text}</p>
 </main>
