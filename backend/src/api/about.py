f03fcb2e8a95c2ef46d44bec137eda80a73b1c7b
lhagen02 <lhagen02@uoguelph.ca>
2023-03-12 15:01:41 -0400

    feat: Use OpenAI api to generate About page text

--- backend/src/api/about.py
+++ backend/src/api/about.py
@@ -1,11 +1,25 @@
 """API route for the About page"""
 
+import os
+import openai
 from flask_restx import Namespace, Resource
 
 api = Namespace('about', description='About page operations')
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 class GenerateAbout(Resource):
     def get(self):
-        return {"message": "universal basic income"}
+        response = openai.Completion.create(
+            model="text-davinci-001",
+            prompt="Tell me about universal basic income.",
+            temperature=0.75,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+
+        return response
 
 api.add_resource(GenerateAbout, "")
--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,17 +1,29 @@
 <script>
+	import { onMount } from 'svelte';
+	let about = "Generating...";
+
+	onMount (async function () {
+		const response = await fetch('http://localhost:8080/api/about');
+		const responseJSON = await response.json();
+
+		about = responseJSON["choices"][0]["text"]
+	});
 </script>
 
-
 <main>
-	<div class="about">
-		<p>Hello, world!</p>
-	</div>
+	<h1 class="px-2 py-4 text-4xl text-slate-500">About Universal Basic Income</h1>
+	<p>{about}</p>
 </main>
 
-<style>
-.about 
-{
-	text-align:center;
 
-}
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.white);
+	}
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 1000px;
+		margin: 0 auto;
+	}
 </style>
