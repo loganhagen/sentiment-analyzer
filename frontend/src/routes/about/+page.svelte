23f30c197f4579c1b4b775b0a0ea0afafca69565
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-16 15:59:44 -0400

    feat: add content to about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,3 +1,49 @@
 <main>
-	<h1 class="text-4xl px-4 py-4">This is the about page.</h1>
+	<img src="https://i.imgur.com/LjZ0Gg5.png" alt="Graphex Icon" style="width:250px;height:300px;">
+	<h1>
+		We are team graphex!
+		
+	</h1>
+	
+	<h1 class="text-4xl px-4 py-4">What is UBI?</h1>
+	<p class="px-8 py-4">Universal basic income is a form of social welfare, people receive supplementary income from the state.</p> 
+		<small class="px-16 py-4">
+			- Full basic income, if it provides above the poverty line.
+		</small>
+		<br>
+		<small class="px-16 py-4">
+			- Partial basic income, any amount below the poverty line.
+		</small>
+			
+		
+
+	<p class="px-8 py-4">
+		
+		No country has implemented either for their full population, however there have been pilot projects all over, including in <a href="https://www.ontario.ca/page/ontario-basic-income-pilot" style="color:blue;">Ontario</a>
+		
+		Some countries have a child benefit that is similar to UBI as it is a buffer income for people taking care of children.
+	</p>
+
+	<h1 class="text-4xl px-4 py-4">Automation</h1>
+	<p class="px-8 py-4">
+		There is an ongoing automation revolution leading to menial jobs being eliminated.
+		Like cashiers in virtually every big box store. People are losing their jobs and 
+		the minimum wage is not being increased in relation to increases in productivity, 
+		at least in North America. Should the state protect workers pushed out of their industry 
+		by offering a supplemental wage? For example, truck drivers in the future with self driving trucks.
+	</p>
+
+	<h1 class="text-4xl px-4 py-4">Living Wage</h1>
+	<p class="px-8 py-4">
+		A living wage is the hourly wage a worker needs to earn to cover their basic expenses and participate in their community. This is not the same as the minimum wage. How do organizations determine the living wage for an area?
+	</p>
+	<a href="https://www.ontariolivingwage.ca/living_wage_week_2022" style="color:blue;" class="px-8 py-4">Ontario's living wage</a>
+	<p class="px-8 py-4">
+		What we consider a living wage is having enough money to have your physiological needs met, as seen in Maslow's hierarchy of basic needs. 
+	</p>
+	<img src="https://i.imgur.com/MBB3Gwk.jpg" alt="basic needs">
+
+
+
+
 </main>
