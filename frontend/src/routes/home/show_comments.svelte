e00c7cd04f83274ab41c427729b255c942967d3e
Jonalton <60904603+Jonalton@users.noreply.github.com>
2023-03-31 10:05:01 -0400

    refactor: move show comments button to its own file

---
+++ frontend/src/routes/home/show_comments.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Post } from '../../types';
+	import { sharedPost } from './store';
+
+	let post: Post;
+	let visible = false;
+
+	//Access post from store whenever the data is changed
+	sharedPost.subscribe((data) => {
+		post = data;
+	});
+
+	//toggle visibility to show the list of comments
+	function toggleVibility() {
+		visible = !visible;
+	}
+	1;
+</script>
+
+<button
+	on:click={toggleVibility}
+	type="button"
+	class="text-left text-xs text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+>
+	Show/Hide Comments({post.comments.length})
+</button>
+{#if visible}
+	<ul class="text-left space-y-5 text-gray-500 list-disc list-inside dark:text-gray-400">
+		{#each post.comments as comment}
+			<li>
+				{comment.text}
+			</li>
+		{/each}
+	</ul>
+{/if}
--- frontend/src/routes/home/show_post.svelte
+++ frontend/src/routes/home/show_post.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
 	import type { Post } from '../../types';
 	import { sharedPost } from './store';
+	import ShowComments from './show_comments.svelte';
 
 	let post: Post;
-	let visible = false;
 
 	//Access post from store whenever the data is changed
 	sharedPost.subscribe((data) => {
 		post = data;
 	});
-
-	function toggleVibility() {
-		visible = !visible;
-	}
 </script>
 
 <div class="rounded max-w-5xl max-y-2 overflow-hidden shadow-lg bg-slate-100 space-y-2 px-4 py-4">
@@ -33,22 +29,7 @@
 				rel="noreferrer">Link to post</a
 			>
 		</p>
-		<button
-			on:click={toggleVibility}
-			type="button"
-			class="left: 0.25rem text-xs text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-		>
-			Show/Hide Comments({post.comments.length})
-		</button>
-		{#if visible}
-			<ul class="text-left space-y-5 text-gray-500 list-disc list-inside dark:text-gray-400">
-				{#each post.comments as comment}
-					<li>
-						{comment.text}
-					</li>
-				{/each}
-			</ul>
-		{/if}
+		<ShowComments />
 	{:else}
 		<p class="text-left font-bold">Source: N/A</p>
 	{/if}
