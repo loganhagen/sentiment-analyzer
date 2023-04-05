b856fd37aa8698b2290ccd3b36d943d6778ff774
klukasze <klukasze@uoguelph.ca>
2023-04-05 19:09:42 -0400

    Refactor: Remove CRON from backend since it refuses to work even though it said it says the jobs are running. Instead, use a bash script that starts our Flask app and fork a child process to manage updating the DB. This child process also updates our DB sentiment plot and puts it in a JSON file so that it can be read and returned by api/plot/sentiment/all.

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,30 +1,11 @@
 """Initializes our API blueprint for our Flask app"""
+import os
 from flask import Blueprint
 from flask_restx import Api
 from src.api.health import api as health_ns
 from src.api.posts import api as posts_ns
 from src.api.plot import api as plot_ns
 from src.api.ask import api as ask_ns
-from src.db.connect import DBConnect
-from src.lib.graphPlotter import GraphPlotter
-from src.lib.languageProcessing import LanguageProcessing
-
-def saveDatabaseSentiment():
-    lp = LanguageProcessing()
-    gp = GraphPlotter()
-    dbc = DBConnect()
-
-    twitter_sentiment, reddit_sentiment = lp.getDatabaseSentiment(dbc)
-
-    twitter_df = lp.sentimentToDataFrame(twitter_sentiment)
-    reddit_df = lp.sentimentToDataFrame(reddit_sentiment)
-
-    db_plot = gp.plotDatabaseSentiment(twitter_df, reddit_df)
-
-    with open("db_sentiment.json", "w", encoding='utf-8') as file:
-        file.write(db_plot)
-        file.close()
-
 
 # Create an API blueprint
 api_bp = Blueprint("api", __name__)
@@ -37,5 +18,3 @@ api.add_namespace(health_ns)
 api.add_namespace(posts_ns)
 api.add_namespace(plot_ns)
 api.add_namespace(ask_ns)
-
-saveDatabaseSentiment()
