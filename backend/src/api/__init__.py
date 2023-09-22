60ed8c4b3ae720afa8e229400e1d71efe69f4008
loganhagen <loganhagen@gmail.com>
2023-09-21 20:57:25 -0400

    fix fetch request url to yoobi service

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -2,10 +2,8 @@
 import os
 from flask import Blueprint
 from flask_restx import Api
-from src.api.health import api as health_ns
 from src.api.posts import api as posts_ns
 from src.api.plot import api as plot_ns
-from src.api.ask import api as ask_ns
 
 # Create an API blueprint
 api_bp = Blueprint("api", __name__)
@@ -14,7 +12,5 @@ api_bp = Blueprint("api", __name__)
 api = Api(api_bp, title="Flask REST API", description="A RESTapi built with Flask")
 
 # Add api namespaces to the api object that was created
-api.add_namespace(health_ns)
 api.add_namespace(posts_ns)
 api.add_namespace(plot_ns)
-api.add_namespace(ask_ns)
